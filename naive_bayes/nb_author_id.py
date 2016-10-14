8f0e8b0214a245811c390e54288f0f5e3768e061
kroot <konstantin.root@gmail.com>
2016-10-14 13:19:48 +0300

    Python 3 related changes, code cleanup

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -11,23 +11,25 @@
 """
     
 import sys
+import os
 from time import time
-sys.path.append("../tools/")
+sys.path.append(os.path.abspath("../tools"))
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import accuracy_score
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+classifier = GaussianNB()
+t0 = time()
+classifier.fit(features_train, labels_train)
+print("training time: ", round(time()-t0, 3), " seconds")
 
+t1 = time()
+predicted = classifier.predict(features_test)
+print("prediction time: ", round(time()-t1, 3), " seconds")
 
-
-
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+score = accuracy_score(labels_test, predicted)
+print(score)

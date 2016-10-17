8905d06d7c6d7f5e30819927b998ab7a8450a1aa
kroot <konstantin.root@gmail.com>
2016-10-17 16:07:05 +0300

    DecisionTree

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,20 +12,24 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.metrics import accuracy_score
 
-
-### features_train and features_test are the features for the training
-### and testing datasets, respectively
-### labels_train and labels_test are the corresponding item labels
+# features_train and features_test are the features for the training
+# and testing datasets, respectively
+# labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+classifier = DecisionTreeClassifier(min_samples_split=40)
 
+print("Used features per entry: ", len(features_train[0]))
 
+t0 = time()
+classifier.fit(features_train, labels_train)
+print("Fit took: ", round(time()-t0, 3), " seconds")
 
+t1 = time()
+predicted = classifier.predict(features_test)
+print("Prediction took: ", round(time()-t1, 3), " seconds")
 
-#########################################################
-### your code goes here ###
-
-
-#########################################################
-
-
+score = accuracy_score(labels_test, predicted)
+print("Score: ", score)

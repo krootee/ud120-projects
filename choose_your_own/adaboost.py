4e18c062b097558e809308aa5142320366ffb127
kroot <konstantin.root@gmail.com>
2016-10-26 17:18:54 +0300

    AdaBoost classifier

---
+++ choose_your_own/adaboost.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python
+
+# import matplotlib.pyplot as plt
+from prep_terrain_data import make_terrain_data
+from class_vis import pretty_picture
+from sklearn.ensemble import AdaBoostClassifier
+from sklearn.metrics import accuracy_score
+
+# the training data (features_train, labels_train) have both "fast" and "slow"
+# points mixed together - separate them so we can give them different colors
+# in the scatter plot and identify them visually
+features_train, labels_train, features_test, labels_test = make_terrain_data(10000)
+
+grade_fast = [features_train[i][0] for i in range(len(features_train)) if labels_train[i] == 0]
+bumpy_fast = [features_train[i][1] for i in range(len(features_train)) if labels_train[i] == 0]
+grade_slow = [features_train[i][0] for i in range(len(features_train)) if labels_train[i] == 1]
+bumpy_slow = [features_train[i][1] for i in range(len(features_train)) if labels_train[i] == 1]
+
+# # initial visualization
+# plt.xlim(0.0, 1.0)
+# plt.ylim(0.0, 1.0)
+# plt.scatter(bumpy_fast, grade_fast, color="b", label="fast")
+# plt.scatter(grade_slow, bumpy_slow, color="r", label="slow")
+# plt.legend()
+# plt.xlabel("bumpiness")
+# plt.ylabel("grade")
+# plt.show()
+
+classifier = AdaBoostClassifier(n_estimators=50, algorithm='SAMME.R')
+classifier.fit(features_train, labels_train)
+predicted_labels = classifier.predict(features_test)
+score = accuracy_score(labels_test, predicted_labels)
+print("Predicted score: ", score)
+
+try:
+    pretty_picture(classifier, features_test, labels_test)
+except NameError:
+    pass
